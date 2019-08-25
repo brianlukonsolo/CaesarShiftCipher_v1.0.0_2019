f9bbbcd72de36ff3f89b4f1ed2720beda80e46fb
brianlukonsolo139 <brian.lukonsolo@gmail.com>
2019-08-25 20:20:36 +0100

    chore(*): Add code to let user decide whether to use encryption or decryption mode. Minor refactor.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,36 +1,80 @@
 import com.brianlukonsolo.CaesarCipher;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("============================================================================");
-        System.out.println("----------------------------------------------------------------------------");
-        System.out.println("                                                                     .d     ");
-        System.out.println("        C A E S A R    S H I F T    C I P H E R                      00     ");
-        System.out.println(" .adPPY1a. 01.dPPY1a.   .adPPY1a. 01.dPPY1a. 01       d0 01.dPPY1a. MM00MMM ");
-        System.out.println("a0P_____00 00P.   .00a a00     00 00P.   0Y0 .01     d0. 00P.    00a  00    ");
-        System.out.println("0PP0000000 00       00 01         00          .01   d0.  00       d0  00    ");
-        System.out.println("01.   .aa 00       00 00a.   .aa 00           .01.d0.   001.   .a00  00.    ");
-        System.out.println(".0Y11d00. 00       00  .0Y11d00. 00             Y00.    00.Y11dP0.   0Y000  ");
-        System.out.println("                                                d0.     00                  ");
-        System.out.println("                                               d0.      00                  ");
-        System.out.println("[01010101010101010101010101010101010101010101010101010101010101010101010101]");
-        System.out.println("[                                                                          ]");
-        System.out.println("[                               [ Version 1.0 ]                            ]");
-        System.out.println("[                          [ Completed: 25/08/2019 ]                       ]");
-        System.out.println("[                         [ Author: Brian Lukonsolo ]                      ]");
-        System.out.println("[                                                                          ]");
-        System.out.println("[01010101010101010101010101010101010101010101010101010101010101010101010101]\n");
-        System.out.println("============================================================================");
-        System.out.println("============================================================================\n");
+        show("============================================================================");
+        show("----------------------------------------------------------------------------");
+        show("                                                                     .d     ");
+        show("        C A E S A R    S H I F T    C I P H E R                      00     ");
+        show(" .adPPY1a. 01.dPPY1a.   .adPPY1a. 01.dPPY1a. 01       d0 01.dPPY1a. MM00MMM ");
+        show("0PP0000000 00       00 01         00          .01   d0.  00       d0  00    ");
+        show(".0Y11d00. 00       00  .0Y11d00. 00             Y00.    00.Y11dP0.   0Y000  ");
+        show("                                                d0.     00                  ");
+        show("                                               d0.      00                  ");
+        show("[01010101010101010101010101010101010101010101010101010101010101010101010101]");
+        show("[                                                                          ]");
+        show("[                               [ Version 1.0 ]                            ]");
+        show("[                          [ Completed: 25/08/2019 ]                       ]");
+        show("[                         [ Author: Brian Lukonsolo ]                      ]");
+        show("[                                                                          ]");
+        show("[01010101010101010101010101010101010101010101010101010101010101010101010101]\n");
+        show(">> Welcome to Brian Lukonsolo's implementation of the Caesar Shift Cipher <<\n");
+        show(" ");
+        show(" ");
 
-        System.out.println(">> Welcome to Brian Lukonsolo's implementation of the Caesar Shift Cipher <<\n");
+        CaesarCipher caesarCipher = new CaesarCipher();
 
-        System.out.println("####--------------------------------------------------------------------####");
-        System.out.println("####====================================================================####");
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (true) {
+                show(">>> Please select a mode: ");
+                show("     -> Press [1] then Enter for - Encryption mode");
+                show("     -> Press [2] then Enter for - Decryption mode");
+                show("    Press Enter or type exit to quit ...");
+                String selectedOption = scanner.nextLine();
+                if (!selectedOption.equals("1") && !selectedOption.equals("2")){
+                    show("Process complete. Exiting program ...");
+                    break;
+                }
 
+                show(">>> Press Enter or type exit to quit ...");
+                show(">>> Please type the message/text then press Enter ...");
+                String message = scanner.nextLine();
+                if (message.toLowerCase().equals("q") || message.toLowerCase().equals("exit") || message.toLowerCase().equals("")){
+                    show("Process complete. Exiting program ...");
+                    break;
+                }
+                show(">>> Please type the shift amount in integer form then press Enter ...");
+                int shiftAmount = Integer.parseInt(scanner.nextLine());
+
+                if (selectedOption.equals("2")){
+                    show(">>> Encrypting message...");
+                    String encryptedText = caesarCipher.encrypt(message, shiftAmount);
+                    show("Encryption complete.\n");
+                    show(">>> Plain text was: " + message);
+                    show(">>> Shift amount was: " + shiftAmount);
+                    show(">>> Encrypted text is: " + encryptedText + "\n");
+                }else{
+                    show(">>> Decrypting message...");
+                    String decryptedText = caesarCipher.decrypt(message, shiftAmount);
+                    show("Decryption complete.\n");
+                    show(">>> Decrypted text was: " + message);
+                    show(">>> Shift amount was: " + shiftAmount);
+                    show(">>> Decrypted text is: " + decryptedText + "\n");
+                }
+
+                show("[01010101010101010101010101010101010101010101010101010101010101010101010101]\n");
+            }
+        } catch(Exception e) {
+            show("Error: " + e.getMessage());
+        }
 
     }
+
+    private static void show(String displayMessage){
+        System.out.println(displayMessage);
+
+    }
+
 }
